02f7100d2f70a2ce44e87997dd5a5178fa06dff4
nikkscode <dominik.kisiala@gmail.com>
2022-08-15 10:01:51 +0200

    Created a limits-ms, that returns the limits set in application-properties

---
+++ src/main/java/com/nikkscode/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,30 @@
+package com.nikkscode.microservices.limitsservice.bean;
+
+public class Limits {
+    int minimum;
+    int maximum;
+
+    public Limits(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    public Limits(){}
+
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+}
